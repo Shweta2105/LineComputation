34e00e7b341e0e7ae21eff034fe9e5e1a5e63864
Shweta2105 <sghadi21@gmail.com>
2021-05-25 15:20:15 +0530

    Length of one line

--- lineComputation/LineCompare.java
+++ lineComputation/LineCompare.java
@@ -2,6 +2,11 @@ public class LineCompare
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Line Comparison computation");
+		int x1=2,x2=8,y1=3,y2=7;
+		double length;
+
+		length=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+		 System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+length);
+
 	}
 }
