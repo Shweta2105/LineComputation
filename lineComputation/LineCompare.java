d74d3ccc146fdc9cbe5160d2e90c8cb833401416
Shweta2105 <sghadi21@gmail.com>
2021-05-26 07:18:02 +0530

    Length calculated

--- lineComputation/LineCompare.java
+++ lineComputation/LineCompare.java
@@ -6,7 +6,7 @@ public class LineCompare
 		double length;
 
 		length=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-		 System.out.println("distancebetween"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+length);
+		 System.out.println("Length of line :"+"("+x1+","+y1+"),"+"("+x2+","+y2+")===>"+length);
 
 	}
 }
